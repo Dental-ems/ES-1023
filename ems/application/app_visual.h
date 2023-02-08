c4f404500c4595eae6f268570549ab61ba4c96b2
Damien <dcardinale@ems-ch.com>
2023-02-08 12:54:06 +0100

    ES-1023_0.0.1_lpc5528_prophecy : struct
    Add files structure to implement the visual module

---
+++ ems/application/app_visual.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+ * @file app_visual.h
+ *
+ *  Created on: 08 february 2023
+ *      Author: dcardinale
+ *
+ *****************************************************************************/
+
+#ifndef APP_VISUAL_H_
+#define APP_VISUAL_H_
+
+#include "../lib/lib_led_mb.h"
+
+void app_visual_init ( void );
+
+#endif /* APP_VISUAL_H_ */
---
+++ ems/lib/lib_led_mb.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+ * @file lib_led_mb.h
+ *
+ *  Created on: 08 february 2023
+ *      Author: dcardinale
+ *
+ *****************************************************************************/
+
+#ifndef LIB_LED_MB_H_
+#define LIB_LED_MB_H_
+
+#include "../drv/drv_io.h"
+
+void lib_led_mb_init ( void );
+
+#endif /* LIB_LED_MB_H_ */
