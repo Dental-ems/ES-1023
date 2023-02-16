bcdb86aa3147b04a27ee851f9129c72d4b0f42a7
Damien <dcardinale@ems-ch.com>
2023-02-16 17:30:39 +0100

    ES-1023_0.2.0_lpc5528_prophecy : dev
    lib:
    add alim switch control

---
+++ ems/lib/lib_alim.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+ * @file lib_alim.h
+ *
+ *  Created on: 16 february 2023
+ *      Author: dcardinale
+ *
+ *****************************************************************************/
+
+#ifndef LIB_ALIM_H_
+#define LIB_ALIM_H_
+
+#include "../drv/drv_io.h"
+
+void lib_alim_init   ( void );
+void lib_alim_enable ( void );
+
+#endif /* LIB_ALIM_H_ */
